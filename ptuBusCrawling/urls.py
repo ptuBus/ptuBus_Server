4cb878f200ec9b44ca567acc578815460e0eeaf9
RokHyung Son <fhrgud1264@gmail.com>
2020-06-22 02:16:50 +0900

    Eliminate the distinction between upper and lower case letters.

--- ptuBusCrawling/urls.py
+++ ptuBusCrawling/urls.py
@@ -8,9 +8,15 @@ from .Views.Train.TrainTimeTableListView import TrainTimeTableListView
 
 urlpatterns = [
     path('Subway/', SubwayListView.as_view()),
+    path('subway/', SubwayListView.as_view()),
     path('SchoolBus/', SchoolBusListView.as_view()),
+    path('schoolbus/', SchoolBusListView.as_view()),
     path('BusTerminal/', BusTerminalListView.as_view()),
+    path('busterminal/', BusTerminalListView.as_view()),
     path('BusTimeTable/', BusTimeTableListView.as_view()),
+    path('bustimetable/', BusTimeTableListView.as_view()),
     path('TrainStation/', TrainStationListView.as_view()),
+    path('trainstation/', TrainStationListView.as_view()),
     path('TrainTimeTable/', TrainTimeTableListView.as_view()),
+    path('traintimetable/', TrainTimeTableListView.as_view()),
 ]
