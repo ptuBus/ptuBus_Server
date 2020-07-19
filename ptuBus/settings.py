13822f4dbe5f81d3695d3087f503e9cc4b3b020d
RokHyung Son <fhrgud1264@gmail.com>
2020-07-19 15:21:07 +0000

    서버 배포용

--- ptuBus/settings.py
+++ ptuBus/settings.py
@@ -134,5 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
