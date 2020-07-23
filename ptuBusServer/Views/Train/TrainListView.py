f16a9b13fa20fc5f23c820feebf2b8ccd2d6d6c8
RokHyung Son <fhrgud1264@gmail.com>
2020-07-24 01:36:06 +0900

    오타 수정

--- ptuBusServer/Views/Train/TrainListView.py
+++ ptuBusServer/Views/Train/TrainListView.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 from ptuBusServer.Serializers import TrainTimeTableSerializer
 from ptuBusServer.Models import TrainTimeTableModel
 from rest_framework import generics
-import urllib.parse
 
 class TrainListView(generics.ListAPIView):
     serializer_class = TrainTimeTableSerializer
@@ -10,14 +8,13 @@ class TrainListView(generics.ListAPIView):
     def get_queryset(self):
         queryset = TrainTimeTableModel.objects.all()
 
-        runDay = self.request.query_params.get('dailyTypeCode', None)
+        runDay = self.request.query_params.get('runDay', None)
         endStationName = self.request.query_params.get('endStationName', None)
-        endStationName = urllib.parse.unquote(endStationName)
         trainClass = self.request.query_params.get('trainClass', None)
         arrivalTime = self.request.query_params.get('arrivalTime', None)
 
         if runDay and endStationName and trainClass and arrivalTime is not None:
-            queryset = queryset.Charfilter(runDay=runDay, endStationName=endStationName,
+            queryset = queryset.filter(runDay=runDay, endStationName=endStationName,
                                        trainClass=trainClass, arrivalTime__startswith=arrivalTime)
         elif runDay and endStationName and trainClass is not None:
             queryset = queryset.filter(runDay=runDay, endStationName=endStationName,
@@ -27,5 +24,5 @@ class TrainListView(generics.ListAPIView):
             queryset = queryset.filter(runDay=runDay, endStationName=endStationName,
                                        arrivalTime__startswith=arrivalTime)
         elif runDay and endStationName is not None:
-            queryset = queryset.filter(runDay=runDay, endStationName__=endStationName)
+            queryset = queryset.filter(runDay=runDay, endStationName=endStationName)
         return queryset
