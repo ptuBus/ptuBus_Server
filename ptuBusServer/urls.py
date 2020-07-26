e9f6c51889e592645aea540b833d37b6373f1dd3
RokHyung Son <fhrgud1264@gmail.com>
2020-07-27 01:00:21 +0900

    URL 경로 수정

--- ptuBusServer/urls.py
+++ ptuBusServer/urls.py
@@ -1,5 +1,4 @@
-
-from django.urls import re_path
+from django.urls import re_path, path
 from .Views import *
 
 urlpatterns = [
@@ -10,7 +9,8 @@ urlpatterns = [
     re_path(r'^subway/$', SubwayTimeTableListView.as_view()),
     re_path(r'^bus/(?P<startStationID>.+)(&P<endStationID>.+)(&P<schedule>\d+)/$', BusTimeTableListView.as_view()),
     re_path(r'^bus/(?P<startStationID>.+)(&P<endStationID>.+)/$', BusTimeTableListView.as_view()),
-    re_path(r'^bus/$', BusTerminalListView.as_view()),
+    re_path(r'^bus/$', BusTimeTableListView.as_view()),
+    path('bus/terminal/', BusTerminalListView.as_view()),
     re_path(r'^schoolbus/(?P<startStationID>.+)(&P<endStationID>.+)(&P<upDownTypeCode>.+)(&P<arrTime>\d+)/$', SchoolBusTimeTableListView.as_view()),
     re_path(r'^schoolbus/(?P<startStationID>.+)(&P<endStationID>.+)(&P<upDownTypeCode>.+)/$', SchoolBusTimeTableListView.as_view()),
     re_path(r'^schoolbus/$', SchoolBusTimeTableListView.as_view()),
@@ -18,5 +18,6 @@ urlpatterns = [
     re_path(r'^train/(?P<runDay>.+)(&P<endStationID>.+)(&P<trainClass>[1-3]+)/$', TrainTimeTableListView.as_view()),
     re_path(r'^train/(?P<runDay>.+)(&P<endStationID>.+)(&P<arrivalTime>\d)/$', TrainTimeTableListView.as_view()),
     re_path(r'^train/(?P<runDay>.+)(&P<endStationID>.+)/$', TrainTimeTableListView.as_view()),
-    re_path(r'^train/$', TrainStationListView.as_view()),
+    re_path(r'^train/$', TrainTimeTableListView.as_view()),
+    path('train/station/', TrainStationListView.as_view())
 ]
